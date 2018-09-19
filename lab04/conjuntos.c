094c369a7000c157a366e8650cfb11d9abf121d6
Francisco Namias Vicente <ra216028@sabbath.lab.ic.unicamp.br>
2018-09-19 12:26:40 -0300

    avancei no lab

--- lab04/conjuntos.c
+++ lab04/conjuntos.c
@@ -8,10 +8,30 @@ typedef struct No{
 } No;
 
 void imprimir_lista(No *lista) {
-    if (lista != NULL) {
-        printf("%d ", lista->num);
+    if (lista != NULL && lista->prox!=NULL) {
+        printf("%d,", lista->num);
         imprimir_lista(lista->prox);
     }
+    else if (lista != NULL && lista->prox==NULL) {
+        printf("%d", lista->num);
+        imprimir_lista(lista->prox);
+    }
+}
+
+void imprimir_conjunto(No *conj) {
+    printf("{");
+    imprimir_lista(conj);
+    printf("}\n");
+}
+
+No *posicao_aux(No *aux, No *novo){
+    if (aux->prox != NULL){
+        if (aux->prox->num < novo->num)
+            return posicao_aux(aux->prox, novo);
+    }
+    else
+        return aux;
+    return aux;
 }
 
 No *Insere(No *novo, No *conj, No *aux){
@@ -24,24 +44,68 @@ No *Insere(No *novo, No *conj, No *aux){
         aux = conj;
         if(aux->num > novo->num){ /*se elemento ja estiver na cabeça da lista*/
             novo->prox = aux;
-            conj = novo;
-            return conj;
+            return novo;
         }
-        while(aux->prox!=NULL)
-            if (aux->prox->num < novo->num)
-                aux = aux->prox;
-            else break;
-        if(aux->prox != NULL){
-            novo->prox = aux->prox;
-            aux->prox = novo;
-        }
-        else{
-            aux->prox = novo;
-            novo->prox = NULL;
-        }
+        aux = posicao_aux(aux, novo); /*acha o local onde será inserida a variavel, recursivamente*/
+        novo->prox = aux->prox;
+        aux->prox = novo;
         return conj;
     }
 }
+
+void Pertence(int num, No *aux){
+    if(aux==NULL){
+        printf("NÃO\n");
+        return;
+    }
+    else if(aux->num == num){
+        printf("SIM\n");
+        return;
+    }
+    else{
+        Pertence(num, aux->prox);
+    }
+}
+
+int pertence_bool(int num, No *aux){
+    if(aux==NULL){
+        return 0;
+    }
+    else if(aux->num == num){
+        return 1;
+    }
+    else{
+        pertence_bool(num, aux->prox);
+    }
+    return 0;
+}
+
+No *Remove(int num, No *conj, No *aux){
+    if(aux==conj && aux->num == num){ /*Numero esta na primeira posição*/
+        return conj->prox;
+    }
+    else if(aux->prox->num == num){
+        No *aux2 = aux->prox;
+        free(aux->prox);
+        aux->prox = aux2->prox;
+        return conj;
+    }
+    else{
+        return(Remove(num, conj, aux->prox));
+    }
+}
+
+No *Uniao(No *conj1, No *conj2, No *aux){
+    if(aux==NULL){
+        return conj1;
+    }
+    else if(!(pertence_bool(aux->num, conj1))) {
+        No *aux2 = NULL;
+        conj1 = Insere(aux, conj1, aux2);
+    }
+    return(Uniao(conj1, conj2, aux->prox));
+}
+
 
 int main(){
     char comando;
@@ -49,7 +113,7 @@ int main(){
     No *conj1 = NULL;
     No *conj2 = NULL;
     No *aux = conj1;
-    scanf("%c", &comando);
+    scanf(" %c", &comando);
     while(comando != 's'){
         if(comando=='i'){
             scanf(" %d%d", &num, &conj);
@@ -60,16 +124,33 @@ int main(){
             else
                 conj2 = Insere(novo, conj2, aux);
         }
-        scanf("%c", &comando);
+        else if(comando=='p'){
+            scanf(" %d%d", &num, &conj);
+            if(conj==1)
+                Pertence(num, conj1);
+            else
+                Pertence(num, conj2);
+        }
+        else if(comando=='r'){
+            scanf(" %d%d", &num, &conj);
+            if(conj==1){
+                aux = conj1;
+                conj1 = Remove(num, conj1, aux);
+            }
+            else{
+                aux = conj1;
+                conj2 = Remove(num, conj2, aux);
+            }
+        }
+        else if(comando=='u'){
+            aux = conj2;
+            conj1 = Uniao(conj1, conj2, aux);
+        }
+
+        imprimir_conjunto(conj1);
+        imprimir_conjunto(conj2);
+        scanf(" %c", &comando);
     }
-    imprimir_lista(conj1);
-    printf("\n");
-    imprimir_lista(conj2);
-    printf("\n");
-    /*else if(comando=='p'){ 
-        scanf(" %d%d", num, conj);
-        Pertence(num, conj);
-    }*/
-    
+
     return 0;
 }
