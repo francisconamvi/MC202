dba506381535923d5aa37a28d94a1ad9b1dc8a11
francisconavy <francisconavy@yahoo.com.br>
2018-09-18 21:59:28 -0300

    acabei de fazer a inserção

--- lab04/conjuntos.c
+++ lab04/conjuntos.c
@@ -1,40 +1,71 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-typedef struct Lista{
+
+typedef struct No{
     int num;
-    struct Lista *proximo;
-} Lista;
+    struct No *prox;
+} No;
 
-void Insere(int num, Lista **conj, Lista **aux){ 
-    Lista *temp = NULL;
-    temp.num = num;
-    *aux = *conj;
-    
-    if(*conj==NULL){
-        *conj = num;
-        *temp->proximo = NULL;
-        *aux = temp;
-        return;
+void imprimir_lista(No *lista) {
+    if (lista != NULL) {
+        printf("%d ", lista->num);
+        imprimir_lista(lista->prox);
     }
-    else if(temp.num < (*aux).num && (*aux)->proximo == NULL){
-        *aux; //isso nao faz nada
+}
+
+No *Insere(No *novo, No *conj, No *aux){
+    if(conj==NULL){ /*se lista for vazia*/
+        conj = novo;
+        novo->prox = NULL;
+        return conj;
+    }
+    else{
+        aux = conj;
+        if(aux->num > novo->num){ /*se elemento ja estiver na cabeça da lista*/
+            novo->prox = aux;
+            conj = novo;
+            return conj;
+        }
+        while(aux->prox!=NULL)
+            if (aux->prox->num < novo->num)
+                aux = aux->prox;
+            else break;
+        if(aux->prox != NULL){
+            novo->prox = aux->prox;
+            aux->prox = novo;
+        }
+        else{
+            aux->prox = novo;
+            novo->prox = NULL;
+        }
+        return conj;
     }
 }
 
 int main(){
     char comando;
     int num, conj;
-    Lista *conj1 = NULL;
-    Lista *conj2 = NULL;
-    Lista *aux;
+    No *conj1 = NULL;
+    No *conj2 = NULL;
+    No *aux = conj1;
     scanf("%c", &comando);
-    if(comando=='i'){
-        scanf(" %d%d", num, conj);
-        if(conj==1)
-            Insere(num, &conj1, &aux);
-        else
-            Insere(num, &conj2, &aux);
+    while(comando != 's'){
+        if(comando=='i'){
+            scanf(" %d%d", &num, &conj);
+            No *novo = (No*) malloc(sizeof(No));
+            novo->num = num;
+            if(conj==1)
+                conj1 = Insere(novo, conj1, aux);
+            else
+                conj2 = Insere(novo, conj2, aux);
+        }
+        scanf("%c", &comando);
     }
+    imprimir_lista(conj1);
+    printf("\n");
+    imprimir_lista(conj2);
+    printf("\n");
     /*else if(comando=='p'){ 
         scanf(" %d%d", num, conj);
         Pertence(num, conj);
